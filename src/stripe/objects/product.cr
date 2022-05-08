73ebf0ff6f338892dc68d948b7f749525e6725f3
Joe Grigg <joe@ghost.org>
2022-05-08 16:59:15 +0100

    Added product update method

--- src/stripe/objects/product.cr
+++ src/stripe/objects/product.cr
@@ -59,4 +59,32 @@ class Stripe::Product
       raise Error.from_json(response.body, "error")
     end
   end
+
+  def self.update(
+    client : Stripe::Client,
+    id : String,
+    name : String? = nil,
+    active : Bool? = nil,
+    description : String? = nil,
+    metadata : Hash? = nil,
+    images : Array(String)? = nil,
+    statement_descriptor : String? = nil,
+    unit_label : String? = nil,
+    type : String? = nil
+  ) : Product forall T, U
+    io = IO::Memory.new
+    builder = ParamsBuilder.new(io)
+
+    {% for x in %w(name active description metadata images statement_descriptor unit_label) %}
+      builder.add({{x}}, {{x.id}}) unless {{x.id}}.nil?
+    {% end %}
+
+    response = client.http_client.post("/v1/products/#{id}", form: io.to_s)
+
+    if response.status_code == 200
+      Product.from_json(response.body)
+    else
+      raise Error.from_json(response.body, "error")
+    end
+  end
 end
